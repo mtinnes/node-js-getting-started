dc985bab9a2830fbccb5b27aa3b365bfb8c3be28
Mike Tinnes <mtinnes@gmail.com>
2021-10-19 17:38:37 -0500

    fix

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use('/', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-//app.use('/', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
-//app.listen(PORT);
 
 // express()
 //   .use(express.static(path.join(__dirname, 'public')))
