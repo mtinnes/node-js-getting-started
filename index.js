16778fba4576a18a7701f562107d7d3df3a33c43
Mike Tinnes <mtinnes@gmail.com>
2021-10-19 18:16:02 -0500

    cleanup

--- index.js
+++ index.js
@@ -4,37 +4,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-// var proxy_options = {
-//   onProxyReq(proxyReq, req, res) {
-//     proxyReq.setHeader( 'Content-Security-Policy', '' );
-//     proxyReq.end();
-//   }
-// };
-
-
 express()
   .use('/', createProxyMiddleware({
     target: 'https://lawsonassociatesinc.thundertix.com/events/display', changeOrigin: true, onProxyRes: function(proxyRes, req, res) {
 
-      //const end = res.end;
-
       proxyRes.headers['Content-Security-Policy'] = '';
       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-
-      // res.end = () => {
-      //   res.setHeader('Content-Security-Policy', '');
-      //   res.setHeader('Access-Control-Allow-Origin', '*');
-
-      //   end.apply(res);
-      // }
     }
   }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
